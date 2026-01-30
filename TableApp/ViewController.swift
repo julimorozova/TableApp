5f876aac602aa14b079946b05c5a3034435deede
Iuliia Morozova <yulii.morozova@gmail.com>
2026-01-30 10:53:43 +0100

    Initial Commit

---
+++ TableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableApp
+//
+//  Created by Julia Morozova on 30. 1. 2026..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
